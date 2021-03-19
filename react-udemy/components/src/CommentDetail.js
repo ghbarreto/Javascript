83d763c44a149e3fa196778671b065ed290f09e4
Gabriel Barreto <gh.barreto@hotmail.com>
2021-03-19 19:25:30 -0300

    components

--- react-udemy/components/src/CommentDetail.js
+++ react-udemy/components/src/CommentDetail.js
@@ -1,20 +1,21 @@
 import React from "react";
-import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
   return (
-    <div className="comment">
-      <a href="/" className="avatar">
-        <img alt="avatar" src="https://source.unsplash.com/random" />
-      </a>
-      <div className="content">
-        <a href="/" className="author">
-          Sam
+    <div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={props.img} />
         </a>
-        <div className="metadata">
-          <span className="date">Today at 3:00 PM</span>
+        <div className="content">
+          <a href="/" className="author">
+            {props.author}
+          </a>
+          <div className="metadata">
+            <span className="date">{props.timeAgo}</span>
+          </div>
+          <div className="text">{props.comments}</div>
         </div>
-        <div className="text">{faker.git.commitMessage()}</div>
       </div>
     </div>
   );
--- react-udemy/components/src/index.js
+++ react-udemy/components/src/index.js
@@ -1,11 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <ApprovalCard>
+        <h4>Warning!</h4>
+        <div>Are you sure you want to do this</div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:00 PM"
+          img={faker.image.image()}
+          comments={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Thais"
+          timeAgo="Yesterday at 3:00 AM"
+          img={faker.image.image()}
+          comments={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Gabriel"
+          timeAgo="Last Wednesday at 4:00 AM"
+          img={faker.image.image()}
+          comments={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
