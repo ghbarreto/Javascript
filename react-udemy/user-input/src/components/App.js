539f5b53aaab76acef46b74b7fcdc7df0d5ba010
Gabriel Barreto <gh.barreto@hotmail.com>
2021-03-25 02:55:19 -0300

    fetching data using axios

--- react-udemy/user-input/src/components/App.js
+++ react-udemy/user-input/src/components/App.js
@@ -1,12 +1,33 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID H7MASXgxc68wVpIykkuoBj-pag2fbeOV-9a19hGDNMs",
+      },
+    });
+    // ? underneath is how to get the values using a promise, above is with async
+    // .then((response) => {
+    //   console.log(response.data.results);
+    // });
+    console.log(response.data.results);
+    this.setState({ images: response.data.results });
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
+      </div>
+    );
+  }
+}
 
 export default App;
--- react-udemy/user-input/src/components/SearchBar.js
+++ react-udemy/user-input/src/components/SearchBar.js
@@ -3,10 +3,17 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  onFormSubmit = (event) => {
+    // Prevents browser from submitting form when pressing enter
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
