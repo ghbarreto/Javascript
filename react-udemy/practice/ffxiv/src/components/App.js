d204881dedb96b41b7650b76e4813a0f24335cdc
Gabriel Barreto <gh.barreto@hotmail.com>
2021-04-01 19:21:29 -0300

    learning

--- react-udemy/practice/ffxiv/src/components/App.js
+++ react-udemy/practice/ffxiv/src/components/App.js
@@ -2,11 +2,7 @@ import React from "react";
 import GetCharacter from "./GetCharacter";
 
 const App = () => {
-  return (
-    <div>
-      <GetCharacter />
-    </div>
-  );
+  return <div>{<GetCharacter />}</div>;
 };
 
 export default App;
--- react-udemy/practice/ffxiv/src/components/GetCharacter.js
+++ react-udemy/practice/ffxiv/src/components/GetCharacter.js
@@ -3,16 +3,20 @@ import xiv from "../api/axios";
 
 const GetCharacter = () => {
   const [val, setData] = useState();
+  const [dname, setName] = useState();
 
+  const handleSubmit = (e) => {
+    console.log(e);
+    setName(e);
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await xiv.get("/character/search?name=Rena+Rose ");
+    const fetchData = async (e) => {
+      const result = await xiv.get(`/character/search?name=${e}`);
       const d = result.data.Results;
       setData(d);
     };
-    fetchData();
-  }, []);
-
+    fetchData(dname);
+  });
   let results = [];
 
   if (val !== undefined) {
@@ -23,42 +27,43 @@ const GetCharacter = () => {
 
   return (
     <div>
-      {results !== undefined
-        ? results.map((e) => {
-            return (
-              <div>
-                <img key={e.ID} src={e.Avatar} />
-                <br />
-                <strong>Feast Matches</strong>: {e.FeastMatches}
-                <br />
-                <strong>ID</strong>: {e.ID}
-                <br />
-                <strong>Language</strong>: {e.Lang}
-                <br />
-                <strong>Name</strong>: {e.Name}
-                <br />
-                <strong>Rank</strong>: {e.Rank}
-                <br />
-                <strong>Rank Icon</strong>: {e.RankIcon}
-                <br />
-                <strong>Server</strong>: {e.Server}
-                <br />
-                <hr />
-              </div>
-            );
-          })
-        : "b"}
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label>Choose a name: </label>
+        <br />
+        <input name="name" type="text" />
+        <button type="button" onClick={(e) => handleSubmit(e.target.value)}>
+          Submit
+        </button>
+      </form>
+      {results !== undefined ? (
+        results.map((e) => {
+          return (
+            <div>
+              <img key={e.ID} src={e.Avatar} />
+              <br />
+              <strong>Feast Matches</strong>: {e.FeastMatches}
+              <br />
+              <strong>ID</strong>: {e.ID}
+              <br />
+              <strong>Language</strong>: {e.Lang}
+              <br />
+              <strong>Name</strong>: {e.Name}
+              <br />
+              <strong>Rank</strong>: {e.Rank}
+              <br />
+              <strong>Rank Icon</strong>: {e.RankIcon}
+              <br />
+              <strong>Server</strong>: {e.Server}
+              <br />
+              <hr />
+            </div>
+          );
+        })
+      ) : (
+        <div>Waiting for a name</div>
+      )}
     </div>
   );
 };
-
-// return <div></div>;
-// (
-//   <div>
-//     {results.map((v) => {
-//       return <div>{v}</div>;
-//     })}
-//   </div>
-// );
 
 export default GetCharacter;
