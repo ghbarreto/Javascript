73e7e33ae04bf85828ea7e028ce6a048cc1b1ea8
Gabriel Barreto <gh.barreto@hotmail.com>
2021-04-01 02:58:25 -0300

    get character done

--- react-udemy/practice/ffxiv/src/api/axios.js
+++ react-udemy/practice/ffxiv/src/api/axios.js
@@ -5,4 +5,14 @@ const KEY = "f9260c55683c42c1966132a0cb28f757fd9ebb8614494792a6eaa944a4110b34";
 export default axios.create({
   baseURL: "https://xivapi.com",
   private_key: KEY,
+  params: {},
+  pagination: {
+    page: 1,
+    page_next: 2,
+    page_prev: false,
+    page_total: 94,
+    results: 250,
+    results_per_page: 2333,
+    results_total: 23500,
+  },
 });
--- react-udemy/practice/ffxiv/src/components/GetCharacter.js
+++ react-udemy/practice/ffxiv/src/components/GetCharacter.js
@@ -2,20 +2,63 @@ import React, { useState, useEffect } from "react";
 import xiv from "../api/axios";
 
 const GetCharacter = () => {
-  const [val, setData] = useState([null]);
+  const [val, setData] = useState();
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await xiv.get("/lore");
+      const result = await xiv.get("/character/search?name=Rena+Rose ");
       const d = result.data.Results;
-      d.map((e) => {
-        console.log(e.Text);
-      });
       setData(d);
     };
-
     fetchData();
   }, []);
-  return <div>{val}</div>;
+
+  let results = [];
+
+  if (val !== undefined) {
+    val.map((e) => {
+      results.push(e);
+    });
+  }
+
+  return (
+    <div>
+      {results !== undefined
+        ? results.map((e) => {
+            return (
+              <div>
+                <img key={e.ID} src={e.Avatar} />
+                <br />
+                <strong>Feast Matches</strong>: {e.FeastMatches}
+                <br />
+                <strong>ID</strong>: {e.ID}
+                <br />
+                <strong>Language</strong>: {e.Lang}
+                <br />
+                <strong>Name</strong>: {e.Name}
+                <br />
+                <strong>Rank</strong>: {e.Rank}
+                <br />
+                <strong>Rank Icon</strong>: {e.RankIcon}
+                <br />
+                <strong>Server</strong>: {e.Server}
+                <br />
+                <hr />
+              </div>
+            );
+          })
+        : "b"}
+    </div>
+  );
 };
+
+// return <div></div>;
+// (
+//   <div>
+//     {results.map((v) => {
+//       return <div>{v}</div>;
+//     })}
+//   </div>
+// );
 
 export default GetCharacter;
