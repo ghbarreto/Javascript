ed40238af6578e438a3ff598cf23518969d662e1
Gabriel Barreto <gh.barreto@hotmail.com>
2021-04-15 11:27:58 -0300

    react-forms learned

--- react-udemy/twitchTv/streams/client/src/index.js
+++ react-udemy/twitchTv/streams/client/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
-import reducers from './reducers'
+import reducers from "./reducers";
 
-const store = createStore(reducers)
+// Applying redux devtools to my application
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancer(applyMiddleware()));
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>
-  ,
-  document.querySelector("#root"));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
